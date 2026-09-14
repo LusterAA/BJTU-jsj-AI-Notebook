df31b93be1dd1830bbb6760ecee7be3850bf3f10
LusterAA <cheng070823@163.com>
2026-09-14 21:58:02 +0800

    更改

--- 本科课程资料/大一上/C语言程序设计/作业/citel/hm16.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main()
-{
-	double A, B;
-	scanf_s("%lf %lf", &A, &B);
-	printf("%.6lf\n", A + B);
-	return 0;
-}
--- 本科课程资料/大一上/C语言程序设计/作业/citel/hm9.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	printf("%d\n",5*6);
-	return 0;
-
-}
